76cb80bb02b5febef34cc7df1ad814c9f29f1eea
rougeni <nicolas.rouge@ensimag.grenoble-int.fr>
2016-09-06 14:59:12 +0200

    signatures fonctions base de données

--- Projet-NET/Data/DataConnection.cs
+++ Projet-NET/Data/DataConnection.cs
@@ -28,6 +28,39 @@ namespace ProjetNET.Data
             return connectionReussi;
         }
 
+        /**
+         * méthode qui récupère les noms et id des différentes fonctions
+         * */
+        public String[,] getIDNames()
+        {
+            return null;
+        }
+
+        /**
+         * méthode qui récupère le nom a partir de l'ID
+         * */
+        public String getName(String ID)
+        {
+            return null;
+        }
+
+        /** 
+         * Méthode qui renvoie l'ID a partir du nom
+         * */
+        public String getID(String nom)
+        {
+            return null;
+        }
+
+        /**
+         * méthode qui donne les cotations pour une action donnée
+         * et pour une durée (nombre de jours ouvrés)
+         * */
+        public int[] getCotation(String ID, int duree)
+        {
+            return null;
+        }
+
         #endregion Public Methods
     }
 }
