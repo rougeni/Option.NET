b317399eff91cd869524527bbbe5418cea49fc39
rougeni <nicolas.rouge@ensimag.grenoble-int.fr>
2016-09-06 13:45:25 +0200

    dataConnection

---
+++ Projet-NET/Data/DataConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNET.Data
+{
+    public class DataConnection
+    {
+        #region Public Methods
+
+        public void ConnectToSql()
+        {
+            System.Data.SqlClient.SqlConnection conn =
+                new System.Data.SqlClient.SqlConnection();
+            // TODO: Modify the connection string and include any
+            // additional required properties for your database.
+            conn.ConnectionString =
+             "integrated security=SSPI;data source=SQL Server Name;" +
+             "persist security info=False;initial catalog=northwind" +
+             "Server=ingefin.ensimag.fr;Database=DotNetDB;User Id=etudiant; Password=edn!2015;";
+            try
+            {
+                conn.Open();
+                // Insert code to process data.
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Failed to connect to data source" + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
+
---
+++ TestUnitaires/TestConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetNET.Data;
+using System.Diagnostics;
+
+namespace TestUnitaires
+{
+    [TestClass]
+    public class TestConnection
+    {
+        [TestMethod]
+        public void TestEtablissementConnection()
+        {
+            DataConnection dataConn = new DataConnection();
+            dataConn.ConnectToSql();
+        }
+    }
+}
