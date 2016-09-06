44d14643d5389bb9c29ea12ba86710034631436c
grunenwflorian <florian.grunenwald1@gmail.com>
2016-09-06 12:25:33 +0200

    Projet corrigé

---
+++ Projet-NET/Data/Action.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet.NET.Data
+{
+    internal class Action
+    {
+        #region Private Fields
+
+        string name;
+        int date;
+        int price;
+
+        #endregion Private Fields
+
+        public string Name { get { return name; } }
+
+        public int Date { get { return date; } }
+
+        public int Price { get { return price; } }
+
+
+    }
+}
---
+++ Projet-NET/Data/IGenerationActionHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet.NET.Data
+{
+    internal interface IGenerationActionHistory
+    {
+        #region Public Methods
+
+        Action[,] CreateActionHistory(int lines, int cols);
+
+        #endregion Public Methods
+    }
+}
