ce781e09c69d8a1c31b60ac3ab03fdb454c35974
grunenwflorian <florian.grunenwald1@gmail.com>
2016-09-06 14:52:49 +0200

    visuel

--- Projet-NET/MainWindowViewModel.cs
+++ Projet-NET/MainWindowViewModel.cs
@@ -5,10 +5,41 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Windows.Threading;
+using ProjetNET.ViewModels;
 
 namespace ProjetNET
 {
     internal class MainWindowViewModel : BindableBase
     {
+        #region Private Fields
+
+        private DispatcherTimer dispatcherTimer;
+        private bool tickerStarted;
+        private IControleViewModel selectedControl;
+        #endregion Private Fields
+
+        public IControleViewModel SelectedControl
+        {
+            get { return selectedControl; }
+            set 
+            {
+                SetProperty(ref selectedControl, value);
+            }
+        }
+
+        public ObservableCollection<IControleViewModel> AvailableControl { get; private set; }
+
+        #region Public Constructors
+
+        public MainWindowViewModel()
+        {
+            IControleViewModel init = new InitControleViewModel();
+            System.Console.WriteLine("init " + init.Name);
+            List<IControleViewModel> myList = new List<IControleViewModel>() {init};
+            AvailableControl = new ObservableCollection<IControleViewModel>(myList);
+        }
+
+        #endregion Public Constructors
+
     }
 }
---
+++ Projet-NET/ViewModels/IControleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNET.ViewModels
+{
+    interface IControleViewModel
+    {
+        #region Public Properties
+
+        string Name { get; }
+
+        void Leave(WholeViewModel wholeView);
+
+        void Arrive(WholeViewModel wholeView);
+
+        #endregion Public Properties
+    }
+}
