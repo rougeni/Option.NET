1b9f1c7f42167b213bb23237554daf4f4c56ddba
grunenwflorian <florian.grunenwald1@gmail.com>
2016-09-07 10:57:24 +0200

    pricing

---
+++ ProjetNET/Data/Action.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNET.Data
+{
+    public class Action
+    {
+        #region Private Fields
+
+        string name;
+        int date;
+        int price;
+
+        #endregion Private Fields
+
+        #region Public Constructor
+
+        public Action(String nom, int date, int prix)
+        {
+            this.name = nom;
+            this.date = date;
+            this.price = prix;
+        }
+
+        #endregion Public Constructor
+
+        public string Name { get { return name; } }
+
+        public int Date { get { return date; } }
+
+        public int Price { get { return price; } }
+
+    }
+}
