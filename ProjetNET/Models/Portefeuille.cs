5234378a87fb4994e5fc27ad9d4aba35fb930670
rougeni <nicolas.rouge@ensimag.grenoble-int.fr>
2016-09-08 13:39:21 +0200

    ajout portefeuille

---
+++ ProjetNET/Models/Portefeuille.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNET.Models
+{
+    class Portefeuille
+    {
+        public DateTime date;
+        public double valeur;
+    }
+}
